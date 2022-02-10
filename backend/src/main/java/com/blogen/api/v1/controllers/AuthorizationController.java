01235e351485968503fb4a969c7602f47ec27827
cliffs <strohs1@gmail.com>
2022-02-09 20:10:51 -0500

    AuthorizationController/username check now returns a boolean response

--- backend/src/main/java/com/blogen/api/v1/controllers/AuthorizationController.java
+++ backend/src/main/java/com/blogen/api/v1/controllers/AuthorizationController.java
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
@@ -73,9 +74,6 @@ public class AuthorizationController {
     public Boolean userNameExists(@PathVariable("name") String userName) {
         Boolean userExists = authorizationService.userNameExists(userName);
         log.debug("check user name exists: {} = {}", userName, userExists);
-//        if (!userExists) {
-//            throw new NotFoundException("user name not found: " + userName);
-//        }
         return userExists;
     }
 }
